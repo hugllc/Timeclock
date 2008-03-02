7b1c9067281c49421e13f24dd7205c80cab38e6a
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2008-03-02 03:05:12 +0000

    Added
    git-svn-id: https://svn.hugllc.com/Timeclock/trunk@1021 b157844d-9f31-0410-aa6b-d326cbe640ab

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#  $Id$
+
+ROOT_DIR=`pwd`
+
+SVN_SERVER=https://svn.hugllc.com/
+
+COM_NAME=$1
+COM_VERSION=$2
+
+if [ x$COM_NAME == "x" ] || [ x$COM_VERSION == "x" ]; then
+    echo "Usage:  $0 <component> <release version>"
+    exit;
+fi
+
+svn commit ./$COM_NAME
+echo Tagging the version
+svn -m "Release $COM_VERSOIN" copy ./$COM_NAME ${SVN_SERVER}/Timeclock/tags/${COM_NAME}/${COM_VERSION}
+
+
+./build.sh $1 $2
