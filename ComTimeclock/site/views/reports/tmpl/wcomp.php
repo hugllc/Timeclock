9207da97a1724814ce28aff3aa6f704ef7ecfbc4
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2008-07-10 20:19:37 +0000

    Tweak for bug #1055.  It shuts off the report and displays an error message if Worker's comp is not enabled.
    git-svn-id: https://svn.hugllc.com/Timeclock/trunk@1523 b157844d-9f31-0410-aa6b-d326cbe640ab

--- ComTimeclock/site/views/reports/tmpl/wcomp.php
+++ ComTimeclock/site/views/reports/tmpl/wcomp.php
@@ -48,6 +48,11 @@ $dateFormat      = JText::_("DATE_FORMAT_LC1");
 $shortDateFormat = JText::_("DATE_FORMAT_LC3");
 $document->setTitle($this->params->get('page_title')." (".JHTML::_('date', $this->period['unix']["start"], $shortDateFormat)." ".JText::_("to")." ".JHTML::_('date', $this->period['unix']["end"], $shortDateFormat).")");
 
+if (!TableTimeclockPrefs::getPref("wCompEnable", "system")) {
+    print '<div class="error"><strong>'.JText::_("This report is not enabled.  Please enable Worker's Compensation.").'</strong></div>';
+    return;
+}
+
 ?>
 
 <form action="<?php JROUTE::_("index.php"); ?>" method="post" name="userform" autocomplete="off">
