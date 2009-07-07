2a70c8776465786fc033a2e1394908a3155a3638
Scott Price <prices@dflytech.com>
2009-07-07 15:48:32 -0500

    Fixed bug #507.  It now installs the tables correctly.

---
+++ ComTimeclock/install.timeclock.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This component is the user interface for the endpoints
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * com_HUGnet is a Joomla! 1.5 component
+ * Copyright (C) 2008-2009 Hunt Utilities Group, LLC
+ * Copyright 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   UI
+ * @package    ComHUGnet
+ * @subpackage Com_HUGnet
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2008-2009 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$
+ * @link       https://dev.hugllc.com/index.php/Project:ComHUGnet
+ */
+
+
+/**
+ * This installs everthing
+ *
+ * @return null
+ */
+function Com_install()
+{
+    $database =& JFactory::getDBO();
+    $adminDir = dirname(__FILE__);
+    foreach (array("HUGnetDisplay.php", "HUGnetDisplay.xml") as $file) {
+        $from = $adminDir.DS."plugins".DS.$file;
+        $to   = JPATH_ROOT.DS."plugins".DS."content".DS.$file;
+        rename($from, $to);
+    }
+    $database->setQuery(
+        "INSERT INTO `#__plugins` "
+        ."(`name`,`element`, `folder`, `access`, `ordering`, "
+        ."`published`, `iscore`, `client_id`, `checked_out`, "
+        ."`checked_out_time`, `params`) "
+        ." VALUES ('Content - HUGnetDisply', 'HUGnetDisplay', "
+        ."'content', 0, 99, 1, 0, 0, 0, '0000-00-00 00:00:00', '');"
+    );
+    $result = $database->query();
+
+    $sql_files = array("hugnet_images", "hugnet_imagepoints", "hugnet_prefs");
+    foreach ($sql_files as $file) {
+        $sql = file_get_contents($adminDir.DS."install".DS.$file.".sql");
+        $database->setQuery($sql);
+        $result = $database->query();
+    }
+
+}
+
+?>
