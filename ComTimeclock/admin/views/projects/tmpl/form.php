6da0afe95e2445773132a94aa669f9c17854fc05
Scott Price <prices@dflytech.com>
2009-07-08 17:08:11 -0500

    Added for bug #2243.

---
+++ ComTimeclock/admin/views/projects/tmpl/form.php
@@ -0,0 +1,267 @@
+<?php
+/**
+ * This component is the user interface for the endpoints
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * com_ComTimeclock is a Joomla! 1.5 component
+ * Copyright (C) 2008 Hunt Utilities Group, LLC
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   UI
+ * @package    ComTimeclock
+ * @subpackage Com_Timeclock
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2008 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$    
+ * @link       https://dev.hugllc.com/index.php/Project:ComTimeclock
+ */
+
+defined('_JEXEC') or die('Restricted access'); 
+jimport("joomla.html.pane");
+
+$title = ($this->add) ? "Add" : "Edit";
+
+TimeclockAdminController::title(JText::_("Project: <small><small>[ ".$title." ]</small></small>"));
+JToolBarHelper::apply();
+JToolBarHelper::save();
+JToolBarHelper::cancel();
+
+?>
+<form action="index.php" method="post" name="adminForm" id="adminForm">
+<?php
+if (!$this->add && ($this->row->type !== "CATEGORY")) {
+    ?>
+    <div style="float: right; width: 30%;">
+    <?php
+    $pane = JPane::getInstance("sliders");
+    echo $pane->startPane("user-pane");  
+    echo $pane->startPanel(JText::_("Add Users"), "adduser-page");
+    ?>
+    <div style="padding: 5px;">
+    <?php 
+        array_shift($this->lists["users"]);
+        print JHTML::_("select.genericList", $this->lists["users"], "user_id[]", 'multiple="multiple"', 'value', 'text', 0); 
+    ?><br />
+        <button onClick="this.form.task.value='adduser';this.form.submit();">Add Users</button>
+    </div>
+    <?php
+    echo $pane->endPanel();
+    echo $pane->startPanel(JText::_("Remove Users"), "removeuser-page");
+    ?>
+    <div style="padding: 5px;">
+    <?php 
+    $options = array(); 
+    foreach ($this->lists["projectUsers"] as $user) {
+         $options[] = JHTML::_("select.option", $user->id, $user->name);
+    } 
+    print JHTML::_("select.genericList", $options, "remove_user_id[]", 'multiple="multiple"', 'value', 'text', 0); 
+        ?><br />
+            <button onClick="this.form.task.value='removeuser';this.form.submit();">Remove Users</button>
+    </div>
+    <?php
+
+    /*
+    foreach ($this->lists["projectUsers"] as $user) { ?>
+                    <button onClick="this.form.task.value='removeuser';this.form.user_id.value='<?php print $user->id;?>';this.form.submit();">Remove</button>
+                    <?php print empty($user->name) ? $user->id : $user->name; ?><br />
+        <?php 
+    } 
+    */
+    ?>
+    </div>
+    <?php
+    echo $pane->endPanel();
+    echo $pane->endPane(); 
+    ?>
+    </div>
+    <?php
+}
+?>
+<div>
+    <table class="admintable">
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="name">
+                    <?php echo JText::_('Name'); ?>:
+                </label>
+            </td>
+            <td>
+                <input class="text_area" type="text" name="name" id="name" size="32" maxlength="64" value="<?php echo $this->row->name;?>" />
+            </td>
+            <td>
+                The name of the project
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key" style="vertical-align: top;">
+                <label for="description">
+                    <?php echo JText::_('Description'); ?>:
+                </label>
+            </td>
+            <td>
+                <textarea class="text_area" type="text" name="description" id="description" cols="30" rows="5"><?php echo $this->row->description;?></textarea>
+            </td>
+            <td>
+                A description of the project
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="Research">
+                    <?php echo JText::_('Manager'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.genericList", $this->lists["allUsers"], "manager", '', 'value', 'text', $this->row->manager);  ?>
+            </td>
+            <td>
+                Who is the project manager?
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="Research">
+                    <?php echo JText::_('Research'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.booleanList", "research", "", $this->row->research); ?>
+            </td>
+            <td>
+                Is this project research?
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="published">
+                    <?php echo JText::_('Active'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.booleanList", "published", "", $this->row->published); ?>
+            </td>
+            <td>
+                Is this project active?
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="type">
+                    <?php echo JText::_('Type'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.genericList", $this->typeOptions, "type", "", 'value', 'text', $this->row->type); ?>
+            </td>
+            <td>
+                The type of project.
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="category">
+                    <?php echo JText::_('Category'); ?>:
+                </label>
+            </td>
+            <td>
+<?php 
+if ($this->row->parent_id < -1) {
+    print JText::_($this->cat->name);
+} else {
+    print JHTML::_("select.genericList", $this->parentOptions, "parent_id", "", 'value', 'text', $this->row->parent_id);
+}
+?>
+            </td>
+            <td>
+                The category the project is in.
+            </td>
+        </tr>
+<?php 
+if ($this->lists["wCompEnable"] != 0) { 
+    ?>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="parent_id">
+                    <?php echo JText::_('Workers Comp Codes'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php for ($i = 1; $i < 7; $i++): ?>
+                <?php $var = "wcCode".$i; ?>
+                <div>
+                    <strong><?php print $i; ?>:</strong>
+                    <?php print JHTML::_("select.genericList", $this->wCompCodeOptions, $var, "", 'value', 'text', (int)$this->row->$var); ?>
+                </div>
+                <?php endfor; ?>
+            </td>
+            <td>
+                The worker's comp codes.  Each code will be listed as a separate hours entry in the timeclock.
+            </td>
+        </tr>
+    <?php
+}
+?>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="customer">
+                    <?php echo JText::_('Customer'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.genericList", $this->lists["customers"], "customer", "", 'value', 'text', (int)$this->row->customer); ?>
+            </td>
+            <td>
+                The customer this project should be billed to
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key" style="vertical-align:top;">
+                <label for="users">
+                    <?php echo JText::_('Users'); ?>:
+                </label>
+            </td>
+            <td style="white-space: nowrap;">
+<?php 
+foreach ($this->lists["projectUsers"] as $user) { ?>
+                    <?php print $user->name; ?><br />
+    <?php
+} 
+?>
+            </td>
+            <td>
+                This is the list of users that are attached to this project.
+            </td>
+        </tr>
+    </table>
+</div>
+
+<div class="clr"></div>
+<input type="hidden" name="created" value="<?php print $this->row->created; ?>" />
+<input type="hidden" name="created_by" value="<?php print $this->row->created_by; ?>" />
+<input type="hidden" name="checked_out" value="<?php print $this->row->checked_out; ?>" />
+<input type="hidden" name="checked_out_time" value="<?php print $this->row->checked_out_time; ?>" />
+
+
+<input type="hidden" name="option" value="com_timeclock" />
+<input type="hidden" name="id" value="<?php print $this->row->id; ?>" />
+<input type="hidden" name="task" value="" />
+<input type="hidden" name="controller" value="projects" />
+</form>
---
+++ ComTimeclock/admin/views/users/tmpl/form.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * This component is the user interface for the endpoints
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * com_ComTimeclock is a Joomla! 1.5 component
+ * Copyright (C) 2008 Hunt Utilities Group, LLC
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   UI
+ * @package    ComTimeclock
+ * @subpackage Com_Timeclock
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2008 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$    
+ * @link       https://dev.hugllc.com/index.php/Project:ComTimeclock
+ */
+
+defined('_JEXEC') or die('Restricted access'); 
+jimport("joomla.html.pane");
+
+TimeclockAdminController::title(JText::_("User Configuration: <small><small>[ ".$this->user->name." ]</small></small>"));
+JToolBarHelper::apply();
+JToolBarHelper::save();
+JToolBarHelper::cancel();
+
+?>
+<form action="index.php" method="post" name="adminForm" id="adminForm">
+<div style="float: right; width: 30%;">
+<?php
+$pane = JPane::getInstance("sliders");
+echo $pane->startPane("project-pane");  
+echo $pane->startPanel(JText::_("Add Projects"), "addproject-page");
+?>
+<div style="padding: 5px;">
+<?php
+array_shift($this->lists["projects"]);
+if (count($this->lists["projects"])) {
+    print JHTML::_("select.genericList", $this->lists["projects"], "projid[]", 'multiple="multiple"', 'value', 'text', 0); 
+    ?><br />
+        <button onClick="this.form.task.value='addproject';this.form.submit();">Add Projects</button>
+    <?php
+} else {
+    print JText::_("No Projects to add");
+}
+?>
+</div>    
+<?php
+echo $pane->endPanel();
+echo $pane->startPanel(JText::_("Add Projects from User"), "adduserproject-page");
+?>
+<div style="padding: 5px;">
+    <?php print JHTML::_("select.genericList", $this->lists["users"], "user_id", 'onChange="this.form.task.value=\'adduserproject\';this.form.submit();"', 'value', 'text', 0); ?>
+</div>    
+<?php
+echo $pane->endPanel();
+echo $pane->startPanel(JText::_("Remove Projects"), "project-page");
+?>
+<div style="padding: 5px;">
+<?php
+$options = array(); 
+foreach ($this->lists["userProjects"] as $proj) {
+     $options[] = JHTML::_("select.option", $proj->id, $proj->name);
+} 
+print JHTML::_("select.genericList", $options, "remove_projid[]", 'multiple="multiple"', 'value', 'text', 0); 
+    ?><br />
+        <button onClick="this.form.task.value='removeproject';this.form.submit();">Remove Projects</button>
+</div>
+<?php
+echo $pane->endPanel();
+echo $pane->endPane(); 
+?>
+</div>
+<div>
+    <table class="admintable">
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="startDate">
+                    <?php echo JText::_('Start Date'); ?>:
+                </label>
+            </td>
+            <td style="white-space:nowrap;">
+                <?php print JHTML::_("calendar", $this->row->startDate, "startDate", "startDate", "%Y-%m-%d", "");?>
+            </td>
+            <td>
+                When this user starts
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="endDate">
+                    <?php echo JText::_('End Date'); ?>:
+                </label>
+            </td>
+            <td style="white-space:nowrap;">
+                <?php print JHTML::_("calendar", $this->row->endDate, "endDate", "endDate", "%Y-%m-%d", "");?>
+            </td>
+            <td>
+                When this user leaves.  Leave blank if the user is still employed.
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="active">
+                    <?php echo JText::_('Active'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.booleanList", "published", "", $this->row->published); ?>
+            </td>
+            <td>
+                Is this user active in the timeclock.  'No' means they will not be able to access any
+                sort of timeclock.
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="reports">
+                    <?php echo JText::_('Reports'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.booleanList", "admin_reports", "", $this->row->prefs["admin_reports"]); ?>
+            </td>
+            <td>
+                Can this user view reports.
+            </td>
+        </tr>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="type">
+                    <?php echo JText::_('User Type'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.genericList", $this->lists["status"], "admin_status", "", 'value', 'text', $this->row->prefs["admin_status"]); ?>
+            </td>
+            <td>
+                The status of the user
+            </td>
+        </tr>
+<?php
+if ($this->row->prefs["admin_status"] == "PARTTIME") {
+    ?>
+        <tr>
+            <td width="100" align="right" class="key">
+                <label for="holidayperc">
+                    <?php echo JText::_('Holiday Pay'); ?>:
+                </label>
+            </td>
+            <td>
+                <?php print JHTML::_("select.integerList", 0, 100, 10, "admin_holidayperc", "", $this->row->prefs["admin_holidayperc"]); ?>%
+            </td>
+            <td>
+                The percentage of holiday pay this user gets
+            </td>
+        </tr>
+    <?php
+}
+?>
+        <tr>
+            <td width="100" align="right" class="key" style="vertical-align:top;">
+                <label for="projects">
+                    <?php echo JText::_('Projects'); ?>:
+                </label>
+            </td>
+            <td style="white-space: nowrap;">
+<?php 
+foreach ($this->lists["userProjects"] as $proj) { ?>
+                    <?php print sprintf("%04d", $proj->id).": ".$proj->name; ?><br />
+    <?php
+} 
+?>
+            </td>
+            <td>
+                This is just a list of projects.
+            </td>
+        </tr>
+        <tr>
+            <td class="key" style="vertical-align:top;">
+                <?php print JText::_("Change History"); ?>:<br />
+                (<?php print JText::_("Old Values"); ?>)
+            </td>
+            <td colspan="2">
+<?php 
+if (!is_array($this->row->history["timestamps"])) $this->row->history["timestamps"] = array();
+krsort($this->row->history["timestamps"]);
+foreach ($this->row->history["timestamps"] as $date => $user) { ?>
+                <p ><div><strong><?php print $user." <br /> ".$date; ?>:</strong></div>
+    <?php
+    foreach ($this->row->history as $key => $value) {
+        if (!array_key_exists($date, $value)) continue;
+        if ($key == "timestamps") continue;
+        print $key." = ".$value[$date]."<br />";
+    } 
+    ?>
+    </p>
+    <?php
+} 
+?>
+            </td>
+        </tr>
+    </table>
+
+</div>
+
+<div class="clr"></div>
+<input type="hidden" name="created" value="<?php print $this->row->created; ?>" />
+<input type="hidden" name="created_by" value="<?php print $this->row->created_by; ?>" />
+<input type="hidden" name="checked_out" value="<?php print $this->row->checked_out; ?>" />
+<input type="hidden" name="checked_out_time" value="<?php print $this->row->checked_out_time; ?>" />
+
+<input type="hidden" name="option" value="com_timeclock" />
+<input type="hidden" name="id" value="<?php print $this->row->id; ?>" />
+<input type="hidden" name="task" id="task" value="" />
+<input type="hidden" name="controller" value="users" />
+</form>
