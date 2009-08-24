3fb82e91ddcd4b94fd6dcdf25c66d5da103e92fc
Scott Price <prices@dflytech.com>
2009-08-24 17:15:28 -0500

    Fixed bug #2507.  I removed the vertical formatting from the table headers.

--- ComTimeclock/site/views/reports/tmpl/default_header.php
+++ ComTimeclock/site/views/reports/tmpl/default_header.php
@@ -7,20 +7,20 @@
  * <pre>
  * com_ComTimeclock is a Joomla! 1.5 component
  * Copyright (C) 2008-2009 Hunt Utilities Group, LLC
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  * </pre>
  *
@@ -30,12 +30,12 @@
  * @author     Scott Price <prices@hugllc.com>
  * @copyright  2008-2009 Hunt Utilities Group, LLC
  * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version    SVN: $Id$    
+ * @version    SVN: $Id$
  * @link       https://dev.hugllc.com/index.php/Project:ComTimeclock
  */
 
-defined('_JEXEC') or die('Restricted access'); 
-$style = "writing-mode: tb-rl; filter: flipv() fliph(); white-space: nowrap; vertical-align: center; height: 1.1em; ";
+defined('_JEXEC') or die('Restricted access');
+//$style = "writing-mode: tb-rl; filter: flipv() fliph(); white-space: nowrap; vertical-align: center; height: 1.1em; ";
 ?>
         <tr>
             <td class="sectiontableheader" style="<?php print $this->cellStyle; ?>">
@@ -45,7 +45,7 @@ $style = "writing-mode: tb-rl; filter: flipv() fliph(); white-space: nowrap; ver
             <td class="sectiontableheader" width="1.1em" style="<?php print $this->cellStyle; ?>">
                 <div style="<?php print $style; ?>"><?php print $user; ?></div>
             </td>
-            <?php endforeach; ?>            
+            <?php endforeach; ?>
             <td class="sectiontableheader" width="1.1em" style="<?php print $this->cellStyle; ?>">
                 <div style="<?php print $style; ?>"><?php print JText::_("Total"); ?></div>
             </td>
