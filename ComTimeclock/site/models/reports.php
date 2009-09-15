934ba3a75415a5ddb745adc0de3a3cfc00296c46
Scott Price <prices@dflytech.com>
2009-09-15 07:08:56 -0500

    Fixed bug #2631.  I hadn't added the round routine to the report model when I stripped it out of the views.  It is now added and works in all models.

--- ComTimeclock/site/models/reports.php
+++ ComTimeclock/site/models/reports.php
@@ -86,6 +86,7 @@ class TimeclockModelReports extends TimeclockModelTimeclock
      */
     function getTimesheetData($where, $limitstart=null, $limit=null, $orderby="")
     {
+        $db = TableTimeclockPrefs::getPref("decimalPlaces", "system");
         if (empty($where)) {
             $where = " 1 ";
         }
@@ -99,11 +100,11 @@ class TimeclockModelReports extends TimeclockModelTimeclock
                 return array();
             }
             foreach ($this->data[$key] as $k => $d) {
-                if ($d->type != "HOLIDAY") {
-                    continue;
+                if ($d->type == "HOLIDAY") {
+                    $hperc = $this->getHolidayPerc($d->user_id, $d->worked);
+                    $this->data[$key][$k]->hours = $d->hours * $hperc;
                 }
-                $hperc = $this->getHolidayPerc($d->user_id, $d->worked);
-                $this->data[$key][$k]->hours = $d->hours * $hperc;
+                $this->data[$key][$k]->hours = round($this->data[$key][$k]->hours, $db);
 
             }
 
