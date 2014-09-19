d6012f8454756f3319bdc048adf308c5dcbbf270
Scott Price <prices@hugllc.com>
2014-09-19 08:57:18 -0500

    Fixed the language files for bug #8373.

--- com_timeclock/site/views/payroll/layouts/toolbar.php
+++ com_timeclock/site/views/payroll/layouts/toolbar.php
@@ -1,7 +1,7 @@
 <div class="toolbar" style="clear: both;">
-    <button class="pull-right" type="button" onclick="Payroll.save();">Save Report</button>
-    <button class="pull-right notlocked" type="button" onclick="Payroll.lock();">Lock Payperiod</button>
-    <button class="pull-right unlock locked" type="button" onclick="Payroll.unlock();">Unlock Payperiod</button>
-    <button class="pull-right livedata noreport" type="button" onclick="Payroll.setReport(false);"><?php print JText::_("COM_TIMECLOCK_SAVED_DATA"); ?></button>
-    <button class="pull-right reportdata noreport" type="button" onclick="Payroll.setReport(true);"><?php print JText::_("COM_TIMECLOCK_LIVE_DATA"); ?></button>
+    <button class="pull-right" type="button" onclick="Payroll.save();"><?php print JText::_("COM_TIMECLOCK_SAVE_REPORT"); ?></button>
+    <button class="pull-right notlocked" type="button" onclick="Payroll.lock();"><?php print JText::_("COM_TIMECLOCK_LOCK_PAYPERIOD"); ?></button>
+    <button class="pull-right unlock locked" type="button" onclick="Payroll.unlock();"><?php print JText::_("COM_TIMECLOCK_UNLOCK_PAYPERIOD"); ?></button>
+    <button class="pull-left livedata noreport" type="button" onclick="Payroll.setReport(false);"><?php print JText::_("COM_TIMECLOCK_SAVED_DATA"); ?></button>
+    <button class="pull-left reportdata noreport" type="button" onclick="Payroll.setReport(true);"><?php print JText::_("COM_TIMECLOCK_LIVE_DATA"); ?></button>
 </div>
