bf09deb5049609ec34086d8ceea8150cc50f3dc3
Scott Price <prices@hugllc.com>
2014-09-30 13:48:58 -0500

    A small change for bug #8376.

--- setversion.sh
+++ setversion.sh
@@ -10,13 +10,13 @@ for file in `find ./${dir} -iname "*.js"|grep -v contrib |grep -v Joomla |grep -
     ${SED} -i'' "s/@version    [ ]*Release[ ~a-z0-9:.$]*/@version    Release: ${1}/g" ${file}
 done
 
-for file in `find ./${dir} -iname "*.php"|grep -v contrib |grep -v Joomla |grep -v "tests/joomla" |grep -v 'bin' |grep -v build |grep -v composer`; do
-    ${SED} -i'' "s/@version    [ ]*SVN[ ~a-z0-9:.$]*/@version    GIT: $Id$/g" ${file}
-done
+#for file in `find ./${dir} -iname "*.php"|grep -v contrib |grep -v Joomla |grep -v "tests/joomla" |grep -v 'bin' |grep -v build |grep -v composer`; do
+#    ${SED} -i'' "s/@version    [ ]*SVN[ ~a-z0-9:.$]*/@version    GIT: $Id$/g" ${file}
+#done
 
-for file in `find ./${dir} -iname "*.js"|grep -v contrib |grep -v Joomla |grep -v "tests/joomla" |grep -v 'bin' |grep -v build |grep -v composer`; do
-    ${SED} -i'' "s/@version    [ ]*SVN[ ~a-z0-9:.$]*/@version    GIT: $Id$/g" ${file}
-done
+#for file in `find ./${dir} -iname "*.js"|grep -v contrib |grep -v Joomla |grep -v "tests/joomla" |grep -v 'bin' |grep -v build |grep -v composer`; do
+#    ${SED} -i'' "s/@version    [ ]*SVN[ ~a-z0-9:.$]*/@version    GIT: $Id$/g" ${file}
+#done
 
 for file in `find ./${dir} -iname "*.xml"|grep -v contrib |grep -v Joomla |grep -v "tests/joomla" |grep -v 'bin' |grep -v build |grep -v composer`; do
     ${SED} -i'' "s/^[[:space:]]*<version>[ ~a-z0-9:.$]*<\/version>/    <version>${1}<\/version>/g" ${file}
