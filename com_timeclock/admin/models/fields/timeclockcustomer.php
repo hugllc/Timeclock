31143aeeefc2650fb12b5448c0fc02d203310f2a
Scott Price <prices@hugllc.com>
2014-10-01 13:18:57 -0500

    Work on bug #8365.  It now allows selecting of a customer for a project.

---
+++ com_timeclock/admin/models/fields/timeclockcustomer.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * This component is the user interface for the endpoints
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * com_ComTimeclock is a Joomla! 1.6 component
+ * Copyright (C) 2014 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   UI
+ * @package    ComTimeclock
+ * @subpackage Com_Timeclock
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2014 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    GIT: $Id: 05067b3e53acbb94ecf90fbfcbca199e94e4cf18 $
+ * @link       https://dev.hugllc.com/index.php/Project:ComTimeclock
+ */
+
+defined('_JEXEC') or die('Restricted access');
+jimport('joomla.form.formfield');
+/**
+ * This creates a select box with the user types in it.
+ *
+ * @category   UI
+ * @package    ComTimeclock
+ * @subpackage Com_Timeclock
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2014 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:ComTimeclock
+ */
+
+class JFormFieldTimeclockCustomer extends JFormField
+{
+    protected $type = 'TimeclockCustomer';
+
+    /**
+    * Method to get the field options.
+    *
+    * @return      array   The field option objects.
+    */
+    public function getInput()
+    {
+        $model = TimeclockHelpersTimeclock::getModel("customer");
+        $depts = $model->listItems(
+            array("c.published=1"),
+            "c.name ASC"
+        );
+        $options = array(
+            JHTML::_(
+                'select.option', 
+                0, 
+                JText::_("JNONE")
+            )
+        );
+        foreach ($depts as $value) {
+            $options[] = JHTML::_(
+                'select.option', 
+                $value->customer_id, 
+                JText::_($value->company)
+            );
+        }
+        return JHTML::_(
+            'select.genericlist', 
+            $options, 
+            $this->name, 
+            array('class' => $this->class), 
+            'value', 
+            'text', 
+            $this->value,
+            $this->id
+        );
+    }
+}
