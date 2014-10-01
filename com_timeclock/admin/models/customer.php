f4967ab8f2298d09bef2b3cdb29016c2dbebd357
Scott Price <prices@hugllc.com>
2014-10-01 14:55:52 -0500

    Made sure all of the incoming database query stuff was properly quoted for bug #8365.

--- com_timeclock/admin/models/customer.php
+++ com_timeclock/admin/models/customer.php
@@ -121,13 +121,13 @@ class TimeclockModelsCustomer extends TimeclockModelsDefault
         }
         $filter = $this->getState("filter");
         if(!empty($filter->search) && is_string($filter->search)) {
-            $query->where("c.company LIKE ".$db->quote("%".$filter->search."%"));
+            $query->where($db->quoteName("c.company")." LIKE ".$db->quote("%".$filter->search."%"));
         }
         if (is_numeric($filter->published)) {
-            $query->where('c.published = ' . (int) $filter->published);
+            $query->where($db->quoteName('c.published').' = ' . $db->quote((int) $filter->published));
         }
         if (is_numeric($filter->user_id)) {
-            $query->where("c.contact_id = " . (int) $filter->user_id);
+            $query->where($db->quoteName("c.contact_id")." = " . $db->quote((int) $filter->user_id));
         }
         return $query;
     }
