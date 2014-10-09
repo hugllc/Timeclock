0b810a094c526cad00837eb179342174764ad892
Scott Price <prices@hugllc.com>
2014-10-09 15:39:43 -0500

    Fixed the zero and empty row stuff for bug #8392.

--- com_timeclock/site/views/billing/layouts/row.php
+++ com_timeclock/site/views/billing/layouts/row.php
@@ -3,6 +3,7 @@ defined('_JEXEC') or die('Restricted access');
 if (!$displayData->hide) :   
     $user_id = $displayData->user_id;
     $cost    = isset($displayData->data["cost"]) ? $displayData->data["cost"] : "0";
+    $hours   = $displayData->data["hours"];
     $places  = $displayData->genparams->get("decimalPlaces");
     $zero    = '<span class="zero">0</span>';
     if (!empty($displayData->data["error"])) {
@@ -13,11 +14,11 @@ if (!$displayData->hide) :
         $errorTooltip = "";
     }
 ?>
-            <tr class="user<?php print $errorClass; ?>"<?php print $errorTooltip; ?>>
+            <tr class="user<?php print $errorClass; ?><?php print empty($hours) ? " empty" : ""; ?>"<?php print $errorTooltip; ?>>
                 <td>
                     <?php print $displayData->name ?>
                 </td>
-                <td class="hours"><?php print $displayData->data["hours"]; ?></td>
+                <td class="hours"><?php print empty($hours) ? $zero : $hours; ?></td>
                 <td class="cost"><?php print $displayData->view->currency($displayData->data["rate"]); ?></td>
                 <td class="total cost"><?php print $displayData->view->currency($cost); ?></span>
                 </td>
