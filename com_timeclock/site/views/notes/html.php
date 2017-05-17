2d52f52648db2c50207c7633c60015a6c19f8e23
Scott L. Price <prices@hugllc.com>
2017-05-17 07:25:46 -0500

    Started working on bug #6.  I am getting the files in place.

---
+++ com_timeclock/site/views/notes/html.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * This component is the user interface for the endpoints
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * com_ComTimeclock is a Joomla! 1.6 component
+ * Copyright (C) 2014 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   UI
+ * @package    ComTimeclock
+ * @subpackage Com_Timeclock
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2014 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    GIT: $Id: e1fc5c887a1edad708ebadc65fbd04a50869766b $
+ * @link       https://dev.hugllc.com/index.php/Project:ComTimeclock
+ */
+/** Check to make sure we are under Joomla */
+defined('_JEXEC') or die('Restricted access');
+
+/** Import the views */
+jimport('joomla.application.component.view');
+
+/**
+ * HTML View class for the ComTimeclockWorld Component
+ *
+ * @category   UI
+ * @package    ComTimeclock
+ * @subpackage Com_Timeclock
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2014 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:ComTimeclock
+ */
+class TimeclockViewsBillingHtml extends JViewHtml
+{
+    /**
+    * Renders this view
+    *
+    * @return unknown
+    */
+    function render()
+    {
+        $app = JFactory::getApplication();
+        $layout = $this->getLayout();
+        
+        $this->params    = JComponentHelper::getParams('com_timeclock');
+        $this->start     = $this->model->getState('start');
+        $this->end       = $this->model->getState('end');
+        $this->report_id = $this->model->getState("report.id");
+        
+        JHTML::stylesheet(
+            JURI::base().'components/com_timeclock/css/timeclock.css', 
+            array(), 
+            true
+        );
+
+        $this->_header   = new JLayoutFile('header', __DIR__.'/layouts');
+        $this->_row      = new JLayoutFile('row', __DIR__.'/layouts');
+        $this->_totals   = new JLayoutFile('totals', __DIR__.'/layouts');
+        $this->_category = new JLayoutFile('category', __DIR__.'/layouts');
+        $this->_toolbar  = new JLayoutFile('toolbar', __DIR__.'/layouts');
+        $this->_export   = new JLayoutFile('export', dirname(__DIR__).'/layouts');
+        $this->_notes    = new JLayoutFile('notes', dirname(__DIR__).'/layouts');
+        $this->_control  = new JLayoutFile('reportcontrol', dirname(__DIR__).'/layouts');
+
+        if (empty($this->report_id)) {
+            $this->data              = $this->model->listItems();
+            $this->users             = $this->model->listUsers();
+            $this->projects          = $this->model->listProjects();
+            $this->filter            = $this->model->getState("filter");
+            $this->filter->start     = $this->start;
+            $this->filter->end       = $this->end;
+            $this->filter->report_id = $this->report_id;
+        } else {
+            $this->report   = $this->model->getReport();
+            $this->data     = $this->report->timesheets;
+            $this->users    = $this->report->users;
+            $this->projects = $this->report->projects;
+            $this->filter   = $this->report->filter;
+            
+            $this->filter->report_id = $this->report_id;
+        }
+        $this->export   = array(
+            "CSV" => "csv",
+            "Excel 2007" => "xlsx",
+        );
+        JHTML::stylesheet(
+            JURI::base().'components/com_timeclock/css/timeclock.css', 
+            array(), 
+            true
+        );
+
+        return parent::render();
+    }
+    /**
+    * This routine formats currency
+    *
+    * @param float $amount The number to format as currency
+    *
+    * @return string The number, formatted as currency
+    */
+    public function currency($amount)
+    {
+        return "$".number_format($amount, 2);
+    }
+}
+?>
---
+++ com_timeclock/site/views/notes/tmpl/report.php
@@ -0,0 +1,70 @@
+<?php
+    JHTML::script(Juri::base()."components/com_timeclock/js/report.js");
+    JHTML::script(Juri::base()."components/com_timeclock/js/timeclock.js");
+    JHTML::_('behavior.modal'); 
+    JHTML::_('behavior.calendar');
+    JHtml::_('formbehavior.chosen', 'select:not(.plain)');
+    $cols = count($this->users) + 2;
+
+    JFactory::getDocument()->setTitle(
+        JText::sprintf(
+            "COM_TIMECLOCK_NOTES_REPORT_TITLE",
+            JHTML::_('date', $this->start, JText::_("DATE_FORMAT_LC3")),
+            JHTML::_('date', $this->end, JText::_("DATE_FORMAT_LC3"))
+        )
+    );
+    $doreports = ($this->report_id != 0);
+?>
+<div id="timeclock" class="container-fluid">
+<form action="<?php JROUTE::_("index.php?option=com_timeclock&controller=notes"); ?>" method="post" name="userform" class="report">
+    <div class="page-header row">
+        <h2 itemprop="name">
+            <a id="timeclocktop"></a>
+            <?php print JText::_("COM_TIMECLOCK_BILLING_REPORT"); ?>
+            <?php print ($doreports) ? " - ".JText::_("COM_TIMECLOCK_SAVED_DATA").":  ".$this->report->name : ""; ?>
+        </h2>
+    </div>
+    <?php print $this->_control->render($this->filter); ?>
+    <div class="dateheader">
+        <strong>
+            <?php print JText::sprintf(
+                "COM_TIMECLOCK_DATE_TO_DATE",
+                JHTML::_('date', $this->start),
+                JHTML::_('date', $this->end)
+                ); ?>
+        </strong>
+    </div>
+    <?php 
+        print $this->_export->render(
+            (object)array(
+                "url" => JRoute::_('&option=com_timeclock&controller=notes'),
+                "export" => $this->export,
+            )
+        ); 
+    ?>
+    <div>
+        <?php 
+            foreach ($this->users as $user_id => $user) {
+                $user->payperiod = $this->payperiod;
+                $user->data = isset($this->data["notes"][$user_id]) ? $this->data["notes"][$user_id] : array();
+                print $this->_notes->render($user);
+            }
+        ?>
+    </div>
+    <input type="hidden" name="controller" value="notes" />
+    <?php print JHTML::_("form.token"); ?>
+</form>
+<script type="text/JavaScript">
+    jQuery( document ).ready(function() {
+        Report.setup();
+    });
+    Report.filter    = <?php print json_encode($this->filter); ?>;
+    Report.projects  = <?php print json_encode($this->projects); ?>;
+    Report.data      = <?php print json_encode($this->data); ?>;
+    Report.doreports = <?php print (int)$doreports; ?>;
+    Timeclock.params = <?php print json_encode($this->params); ?>;
+    Timeclock.report = 0;
+    
+
+</script>
+</div>
