619765a7b93d9f261250fba0981ed50a0c3530e5
Scott Price <prices@hugllc.com>
2017-05-18 08:29:15 -0500

    This fixes #4.  It will now submit the data and reload the page when the project is changed.

--- com_timeclock/admin/js/edit.js
+++ com_timeclock/admin/js/edit.js
@@ -11,7 +11,7 @@ Joomla.Timeclock.submitbutton = function (task, controller)
     } else if (task == "cancel") {
         jQuery('#adminForm [name="task"]').val("cancel");
         jQuery("#adminForm").submit();
-    } else if (task == "apply") {
+    } else if ((task == "apply") || (task == "update")) {
         var data = {};
         jQuery("#adminForm :input").each(function(ind,elem){
             data[jQuery(elem).attr('name')] = jQuery(elem).val();
@@ -24,7 +24,7 @@ Joomla.Timeclock.submitbutton = function (task, controller)
             dataType: 'JSON',
             success: function(data)
             {
-                console.log(data);
+                //console.log(data);
                 if ( data.success ){
                     // This puts the new values into the form.  This is required
                     // for new records, as the primary key must be set or another
@@ -35,6 +35,9 @@ Joomla.Timeclock.submitbutton = function (task, controller)
                         jQuery('[name="'+key+'"]').val(data.data[key]);
                     }
                     Joomla.renderMessages({message: [data.message]});
+                    if (task == "update") {
+                        location.reload(false);
+                    }
                 } else {
                     Joomla.renderMessages({error: [data.message]});
                 }
