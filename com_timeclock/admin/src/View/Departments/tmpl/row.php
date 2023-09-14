0c841a27b997d4aef14dbd8600d2868aa1b88ca9
Scott L. Price <prices@hugllc.com>
2023-09-13 21:40:32 -0500

    Fixed the editing of items locked by current user.

--- com_timeclock/admin/src/View/Departments/tmpl/row.php
+++ com_timeclock/admin/src/View/Departments/tmpl/row.php
@@ -10,6 +10,8 @@ $canEdit    = $user->authorise('core.edit',       'com_timeclock');
 $canCheckin = $user->authorise('core.manage',     'com_checkin') || $displayData["data"]->checked_out == $userId || $displayData["data"]->checked_out == 0;
 $canEditOwn = $user->authorise('core.edit.own',   'com_timeclock') && $displayData["data"]->created_by == $user->id;
 $canChange  = $user->authorise('core.edit.state', 'com_timeclock') && $canCheckin;
+$checkedOut = $displayData["data"]->checked_out && ($displayData["data"]->checked_out != $user->id);
+
 ?>
                 <tr class="row<?php echo $displayData["index"] % 2; ?>" sortable-group-id="<?php echo $displayData["data"]->department_id?>">
                     <td class="center">
@@ -25,7 +27,7 @@ $canChange  = $user->authorise('core.edit.state', 'com_timeclock') && $canChecki
                             <?php if ($displayData["data"]->checked_out) : ?>
                                 <?php echo HTMLHelper::_('jgrid.checkedout', $displayData["index"], $displayData["data"]->checked_out, $displayData["data"]->checked_out_time, 'department.', $canCheckin); ?>
                             <?php endif; ?>
-                            <?php if (($canEdit || $canEditOwn) && !($displayData["data"]->checked_out)): ?>
+                            <?php if (($canEdit || $canEditOwn) && !$checkedOut): ?>
                                 <a href="<?php echo Route::_('index.php?option=com_timeclock&controller=department&task=edit&department_id='.(int) $displayData["data"]->department_id); ?>">
                                 <?php echo $displayData["data"]->name; ?></a>
                             <?php else : ?>
