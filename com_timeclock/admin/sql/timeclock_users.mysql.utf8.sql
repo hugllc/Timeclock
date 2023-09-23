5bd966b1ec6e3d5e095f5bb5fccb39f2a1ca9bd3
Scott Price <prices@hugllc.com>
2023-09-23 14:31:56 -0500

    Fixed and updated the SQL insert statements.

--- com_timeclock/admin/sql/timeclock_users.mysql.utf8.sql
+++ com_timeclock/admin/sql/timeclock_users.mysql.utf8.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `#__timeclock_users` (
-  `project_id` int(11) NOT NULL default '0',
-  `user_id` int(11) NOT NULL default '0',
+  `project_id` int NOT NULL default '0',
+  `user_id` int NOT NULL default '0',
   UNIQUE KEY `UNIQUE` (`project_id`,`user_id`)
 ) DEFAULT CHARSET=utf8;
--- com_timeclock/admin/sql/upgrade.mysql.utf8.sql
+++ com_timeclock/admin/sql/upgrade.mysql.utf8.sql
@@ -1,34 +1,7 @@
 
-## This is for the timesheet table
-
-ALTER TABLE `tqlf3_timeclock_timesheet` CHANGE `id` `timesheet_id` INT( 11 ) NOT NULL DEFAULT '0';
-ALTER TABLE `tqlf3_timeclock_timesheet` CHANGE `created_by` `user_id` INT( 11 ) NOT NULL DEFAULT '0';
-ALTER TABLE `tqlf3_timeclock_timesheet` ADD `created_by` INT NOT NULL DEFAULT '0' AFTER `worked` ;
-
-ALTER TABLE `tqlf3_timeclock_customers` CHANGE `id` `customer_id` INT( 11 ) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `tqlf3_timeclock_customers` ADD `contact_id` INT NOT NULL DEFAULT '0' AFTER `published`;
-ALTER TABLE `tqlf3_timeclock_customers` ADD `modified` DATETIME NOT NULL AFTER `created`;
-
-ALTER TABLE `tqlf3_timeclock_projects` CHANGE `id` `project_id` INT( 11 ) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `tqlf3_timeclock_projects` ADD `modified` DATETIME NOT NULL AFTER `created`;
-
-ALTER TABLE `tqlf3_timeclock_projects` CHANGE `manager` `manager_id` INT( 11 ) NOT NULL DEFAULT '0';
-ALTER TABLE `tqlf3_timeclock_projects` CHANGE `customer` `customer_id` INT( 11 ) NOT NULL DEFAULT '0';
-ALTER TABLE `tqlf3_timeclock_projects` ADD `department_id` INT NOT NULL DEFAULT '0' AFTER `customer_id`;
-
-ALTER TABLE `tqlf3_timeclock_timesheet` CHANGE `id` `timesheet_id` INT( 11 ) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `tqlf3_timeclock_timesheet` CHANGE `created_by` `user_id` INT( 11 ) NOT NULL DEFAULT '0';
-ALTER TABLE `tqlf3_timeclock_timesheet` ADD `created_by` INT NOT NULL DEFAULT '0' AFTER `worked`;
-ALTER TABLE `tqlf3_timeclock_timesheet` ADD `modified` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `created`;
-
-ALTER TABLE `tqlf3_timeclock_users` CHANGE `id` `project_id` INT( 11 ) NOT NULL DEFAULT '0';
-
-ALTER TABLE `tqlf3_timeclock_projects` CHANGE `type` `type` ENUM('PROJECT','CATEGORY','PTO','HOLIDAY','UNPAID','FLOATING_HOLIDAY') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'PROJECT';
-ALTER TABLE `tqlf3_timeclock_projects` CHANGE `created` `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, CHANGE `checked_out_time` `checked_out_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `tqlf3_timeclock_projects` ADD `max_daily_hours` INT NOT NULL DEFAULT '0' AFTER `department_id`, ADD `min_daily_hours` INT NOT NULL DEFAULT '0' AFTER `max_daily_hours`, ADD `max_yearly_hours` INT NOT NULL DEFAULT '0' AFTER `min_daily_hours`;
-
 ## This is for 0.9.14
-ALTER TABLE `tqlf3_timeclock_customers` CHANGE `checked_out` `checked_out` INT(11) NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL, CHANGE `created` `created` DATETIME NULL DEFAULT NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL;
-ALTER TABLE `tqlf3_timeclock_departments` CHANGE `checked_out` `checked_out` INT(11) NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL;
-ALTER TABLE `tqlf3_timeclock_projects` CHANGE `created` `created` DATETIME NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL, CHANGE `checked_out` `checked_out` INT NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL; ALTER TABLE `tqlf3_timeclock_pto` CHANGE `created_by` `created_by` INT(11) NULL DEFAULT NULL, CHANGE `created` `created` DATETIME NULL DEFAULT NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL, CHANGE `checked_out` `checked_out` INT(11) NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL;
-ALTER TABLE `tqlf3_timeclock_timesheet` CHANGE `worked` `worked` DATE NOT NULL, CHANGE `created_by` `created_by` INT NOT NULL, CHANGE `created` `created` DATETIME NOT NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL, CHANGE `checked_out` `checked_out` INT NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `#__timeclock_customers` CHANGE `checked_out` `checked_out` INT NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL, CHANGE `created` `created` DATETIME NULL DEFAULT NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `#__timeclock_departments` CHANGE `checked_out` `checked_out` INT NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `#__timeclock_projects` CHANGE `created` `created` DATETIME NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL, CHANGE `checked_out` `checked_out` INT NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL; ALTER TABLE `asdf_timeclock_pto` CHANGE `created_by` `created_by` INT NULL DEFAULT NULL, CHANGE `created` `created` DATETIME NULL DEFAULT NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL, CHANGE `checked_out` `checked_out` INT NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `#__timeclock_timesheet` CHANGE `worked` `worked` DATE NOT NULL, CHANGE `created_by` `created_by` INT NOT NULL, CHANGE `created` `created` DATETIME NOT NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL, CHANGE `checked_out` `checked_out` INT NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `#__timeclock_pto` CHANGE `created` `created` DATETIME NOT NULL, CHANGE `modified` `modified` DATETIME NULL DEFAULT NULL, CHANGE `checked_out` `checked_out` INT NULL DEFAULT NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL DEFAULT NULL; 
