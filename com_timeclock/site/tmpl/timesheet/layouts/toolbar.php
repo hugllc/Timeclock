71cfa92d341838f54a63157cbbde9feca14f2205
Scott L. Price <prices@hugllc.com>
2023-11-24 11:27:22 -0600

    This fixes #26.  There is now a button to go back to the payroll report.

--- com_timeclock/site/tmpl/timesheet/layouts/toolbar.php
+++ com_timeclock/site/tmpl/timesheet/layouts/toolbar.php
@@ -2,8 +2,11 @@
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
+use HUGLLC\Component\Timeclock\Administrator\Helper\TimeclockHelper;
+
 defined('_JEXEC') or die(); 
 $url = "index.php?option=com_timeclock&controller=timesheet&id=".$displayData->user->id."&date=".$displayData->payperiod->start;
+$payroll = "index.php?option=com_timeclock&view=payroll&date=".$displayData->payperiod->start;
 ?>
 <div class="toolbar" style="clear: both;">
 <?php if ($displayData->user->me && !$displayData->payperiod->approved): ?>
@@ -14,5 +17,8 @@ $url = "index.php?option=com_timeclock&controller=timesheet&id=".$displayData->u
     <?php else: ?>
     <button class="pull-right notapproved" type="button" onclick="window.location.href='<?php print $url; ?>&task=approve';"><?php print Text::_("COM_TIMECLOCK_TIMESHEET_APPROVE"); ?></button>
     <?php endif; ?>
+<?php endif; ?>
+<?php if (!$displayData->user->me && TimeclockHelper::getActions()->get("timeclock.payroll")): ?>
+    <button class="pull-right" type="button" onclick="window.location.href='<?php print $payroll; ?>';"><?php print Text::_("COM_TIMECLOCK_PAYROLL_REPORT"); ?></button>
 <?php endif; ?>
 </div>
