dfe80b23c976a49c7d1eccaac4faed7860039d75
Scott L. Price <prices@hugllc.com>
2025-04-15 01:29:40 +0000

    Approve vs. approve.all works.

--- com_timeclock/site/tmpl/timesheet/layouts/toolbar.php
+++ com_timeclock/site/tmpl/timesheet/layouts/toolbar.php
@@ -6,13 +6,15 @@ use HUGLLC\Component\Timeclock\Administrator\Helper\TimeclockHelper;
 use Joomla\CMS\Router\Route;
 
 defined('_JEXEC') or die(); 
-$url = Route::_("index.php?controller=timesheet&id=".$displayData->user->id."&date=".$displayData->payperiod->start);
+$url = Route::_("index.php?option=com_timeclock&controller=timesheet&id=".$displayData->user->id."&date=".$displayData->payperiod->start);
 $payroll = Route::_("index.php?option=com_timeclock&view=payroll&date=".$displayData->payperiod->start);
+$approval = $displayData->me->authorise("timeclock.timesheet.approve.all", "com_timeclock") 
+  || ($displayData->me->authorise("timeclock.timesheet.approve", "com_timeclock") && ((int)$displayData->user->timeclock["manager"] == (int)$displayData->me->id));
 ?>
 <div class="toolbar" style="clear: both;">
 <?php if ($displayData->user->me && !$displayData->payperiod->approved): ?>
     <button class="pull-right notcomplete" type="button" onclick="Timesheet.complete();" style="display: none;"><?php print Text::_("COM_TIMECLOCK_TIMESHEET_COMPLETE"); ?></button>
-<?php elseif (!$displayData->user->me && $displayData->me->authorise("timeclock.timesheet.approve", "com_timeclock") && !$displayData->payperiod->locked): ?>
+<?php elseif (!$displayData->user->me && $approval && !$displayData->payperiod->locked): ?>
     <?php if ($displayData->payperiod->approved): ?>
     <button class="pull-right approved" type="button" onclick="window.location.href='<?php print $url; ?>&task=disapprove';"><?php print Text::_("COM_TIMECLOCK_TIMESHEET_DISAPPROVE"); ?></button>
     <?php else: ?>
